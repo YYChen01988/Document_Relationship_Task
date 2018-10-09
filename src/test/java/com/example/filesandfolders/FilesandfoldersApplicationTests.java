d20eaffedf35d351d17937c80dd823185ccd1625
yingying <chen.yingying.01988@gmail.com>
2018-10-09 16:38:41 +0100

    test passed createFileAndFolderThenSave

--- src/test/java/com/example/filesandfolders/FilesandfoldersApplicationTests.java
+++ src/test/java/com/example/filesandfolders/FilesandfoldersApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.filesandfolders;
 
+import com.example.filesandfolders.models.File;
+import com.example.filesandfolders.models.Folder;
 import com.example.filesandfolders.repositories.FileRepo;
 import com.example.filesandfolders.repositories.FolderRepo;
 import org.junit.Test;
@@ -21,6 +23,16 @@ public class FilesandfoldersApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void createFileAndFolderThenSave() {
 
+		Folder folder = new Folder("Folder1");
+		folderRepo.save(folder);
 
+		File file1 = new File("name", "rb", "125kb", folder);
+		fileRepo.save(file1);
+
+		folder.addFile(file1);
+		folderRepo.save(folder);
+	}
 }
