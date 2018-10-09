5f65db719c2f89502a3074d62a2a1f45def67446
yingying <chen.yingying.01988@gmail.com>
2018-10-09 16:31:04 +0100

    Create folder class and folderRepo

--- src/main/java/com/example/filesandfolders/models/Folder.java
+++ src/main/java/com/example/filesandfolders/models/Folder.java
@@ -1,4 +1,61 @@
 package com.example.filesandfolders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
 public class Folder {
+
+    //A Folder should have:
+    //a title
+    //list of files
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+        this.files = new ArrayList<File>();
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public void addFile(File file){
+        this.files.add(file);
+    }
 }
---
+++ src/main/java/com/example/filesandfolders/repositories/FolderRepo.java
@@ -0,0 +1,9 @@
+package com.example.filesandfolders.repositories;
+
+import com.example.filesandfolders.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepo extends JpaRepository<Folder, Long> {
+}
